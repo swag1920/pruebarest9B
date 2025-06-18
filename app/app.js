9305fcef22ff06b6824c69a15b4fa2bf725763da
swag1920 <139483442+swag1920@users.noreply.github.com>
2025-06-18 17:52:29 -0600

    se realiza el primer router del api

--- app/app.js
+++ app/app.js
@@ -1,5 +1,8 @@
 
 const express = require('express');
 const app = express();
-
+const router = require('./routes/gymRouter')
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+app.use('/gym', router)
 module.exports=app;
