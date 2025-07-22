b54561d0da426736b3e565b7e17591fb75d483f4
swag1920 <139483442+swag1920@users.noreply.github.com>
2025-07-22 17:42:11 -0600

    Se agregó el CRUD completo con funciones de crear, buscar, eliminar y actualizar.

--- app/controllers/ejercicioController.js
+++ app/controllers/ejercicioController.js
@@ -1,5 +1,6 @@
 const ejercicioModel = require('../models/EjerciciosModel');
 
+
 function buscarTodo(req, res) {
     ejercicioModel.find({})
         .then(ejercicios => {
@@ -9,10 +10,110 @@ function buscarTodo(req, res) {
             return res.status(204).send({ mensaje: "No hay información que mostrar" });
         })
         .catch(e => {
-            return res.status(404).send({ mensaje: "Error al buscar la información: ${e} "});
+            return res.status(404).send({ mensaje: `Error al buscar la información: ${e}` });
+        });
+}
+
+
+function guardarEjercicio(req, res) {
+    console.log(req.body);
+    new ejercicioModel(req.body).save()
+        .then(info => {
+            return res.status(200).send({ mensaje: "Información guardada con éxito", info });
+        })
+        .catch(e => {
+            return res.status(404).send({ mensaje: "Error al guardar la información", e });
+        });
+}
+
+
+function buscarEjercicio(req, res, next) {
+    let consulta = {};
+    consulta[req.params.key] = req.params.value;
+
+    ejercicioModel.find(consulta)
+        .then(info => {
+            if (!info.length) return next();
+            res.locals.ejercicios = info;
+            return next();
+        })
+        .catch(e => {
+            res.locals.e = e;
+            next();
+        });
+}
+
+
+function mostrarEjercicio(req, res) {
+    if (res.locals.e) {
+        return res.status(404).send({
+            mensaje: "Error al buscar la información",
+            error: res.locals.e
+        });
+    }
+
+    if (!res.locals.ejercicios) {
+        return res.status(204).send({ mensaje: "No hay información que mostrar" });
+    }
+
+    return res.status(200).send({ ejercicios: res.locals.ejercicios });
+}
+
+
+function eliminarEjercicio(req, res) {
+    if (res.locals.e) {
+        return res.status(404).send({
+            mensaje: "Error al buscar la información",
+            error: res.locals.e
+        });
+    }
+
+    if (!res.locals.ejercicios || res.locals.ejercicios.length === 0) {
+        return res.status(204).send({ mensaje: "No hay información que eliminar" });
+    }
+
+    const consulta = {};
+    consulta[req.params.key] = req.params.value;
+
+    ejercicioModel.findOneAndDelete(consulta)
+        .then(info => {
+            return res.status(200).send({ mensaje: "Información eliminada", info });
+        })
+        .catch(e => {
+            return res.status(500).send({ mensaje: "Error al eliminar la información", e });
+        });
+}
+
+
+function actualizarEjercicio(req, res) {
+    if (res.locals.e) {
+        return res.status(404).send({
+            mensaje: "Error al buscar la información",
+            error: res.locals.e
+        });
+    }
+
+    if (!res.locals.ejercicios || res.locals.ejercicios.length === 0) {
+        return res.status(204).send({ mensaje: "No hay información que actualizar" });
+    }
+
+    const consulta = {};
+    consulta[req.params.key] = req.params.value;
+
+    ejercicioModel.findOneAndUpdate(consulta, req.body, { new: true })
+        .then(info => {
+            return res.status(200).send({ mensaje: "Información actualizada", info });
+        })
+        .catch(e => {
+            return res.status(500).send({ mensaje: "Error al actualizar la información", e });
         });
 }
 
 module.exports = {
-    buscarTodo
+    buscarTodo,
+    guardarEjercicio,
+    buscarEjercicio,
+    mostrarEjercicio,
+    eliminarEjercicio,
+    actualizarEjercicio
 };
--- app/routes/gymRouter.js
+++ app/routes/gymRouter.js
@@ -1,6 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const ejercicioController = require('../controllers/ejercicioController')
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/ejercicioController'); 
 
-router.get('/ejercicios', ejercicioController.buscarTodo)
-module.exports=router
+router.get('/ejercicios', controller.buscarTodo)
+      .post('/ejercicios', controller.guardarEjercicio)
+      .get('/ejercicios/:key/:value', controller.buscarEjercicio, controller.mostrarEjercicio)
+      .delete('/ejercicios/:key/:value', controller.buscarEjercicio, controller.eliminarEjercicio)
+      .put('/ejercicios/:key/:value', controller.buscarEjercicio, controller.actualizarEjercicio);
+
+module.exports = router;
